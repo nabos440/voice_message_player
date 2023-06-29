4262344baa289c5d3f36cd9a043c96f975e9f879
nabos440 <104625739+nabos440@users.noreply.github.com>
2023-06-29 19:59:52 +0500

    Update voice_message.dart

--- lib/src/voice_message.dart
+++ lib/src/voice_message.dart
@@ -314,13 +314,9 @@ class _VoiceMessageState extends State<VoiceMessage>
 
   void _setDuration() async {
     if (widget.duration != null) {
-      setState((){
       _audioDuration = widget.duration;
-      });
     } else {
-       
       _audioDuration = await jsAudio.AudioPlayer().setUrl(widget.audioSrc!);
-       
     }
     duration = _audioDuration!.inMilliseconds;
     maxDurationForSlider = duration + .0;
